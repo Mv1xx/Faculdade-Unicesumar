17f83271d4c45693c021364c0c5d7dbfe65e19bc
Maria <mariaclaravskis.@gmail.com>
2026-06-16 23:58:49 -0300

    add Trabalho Final

---
+++ 1. Período/FrontEnd/20260602 - Trabalho Final/js/navbar.js
@@ -0,0 +1,10 @@
+const navbar = document.getElementById('navbar');
+navbar.innerHTML = `
+    <nav class="links">
+        <a href="index.html">Início</a>
+        <a href="cadastro.html">Cadastro</a>
+        <a href="notas.html">Notas</a>
+        <a href="relatorios.html">Relatórios</a>
+        <a href="sobre.html">Sobre</a>
+    </nav>
+`;
